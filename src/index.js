a31eecfee7035da6575121175b26702c4e4e7158
uday <udaybikkinauday54359@gmail.com>
2024-08-11 17:01:57 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Header/>
   </React.StrictMode>
 );
 
